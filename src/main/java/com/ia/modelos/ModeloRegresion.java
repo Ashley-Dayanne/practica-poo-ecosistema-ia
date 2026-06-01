e495057fec2efbc407c4a2fd3b9f8ce69726f8a5
Ashley-Dayanne <ux24ii240@ux.edu.mx>
2026-05-31 21:15:20 -0600

    feat: add ModeloRegresion specialization

---
+++ src/main/java/com/ia/modelos/ModeloRegresion.java
@@ -0,0 +1,30 @@
+package com.ia.modelos;
+
+public class ModeloRegresion extends ModeloIA {
+
+```
+private double coeficienteRegularizacion;
+
+public ModeloRegresion(String nombre,
+                       double tasaAprendizaje,
+                       double coeficienteRegularizacion) {
+
+    super(nombre, tasaAprendizaje);
+    this.coeficienteRegularizacion = coeficienteRegularizacion;
+}
+
+@Override
+public void mostrarMetricas() {
+
+    super.mostrarMetricas();
+
+    System.out.println(
+            "Coeficiente de regularización: "
+            + coeficienteRegularizacion
+    );
+
+    System.out.println("==============================");
+}
+```
+
+}
